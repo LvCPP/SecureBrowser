579655533c38563865d75fa2ed941fa765f88058
Andriy Yushchenko <1111andrey1111@gmail.com>
2017-04-20 15:43:42 +0300

    Fixed indents

--- LoggerTest/main.cpp
+++ LoggerTest/main.cpp
@@ -6,13 +6,13 @@ using namespace SecureBrowser;
 
 int main()
 {
-	//Logger inst;									//write to std::cout with min_level DEBUG (lowest level)
+	//Logger inst;					//write to std::cout with min_level DEBUG (lowest level)
 
-	Logger inst(LogLevel::Info);					//write to std::cout with min_level INFO
+	Logger inst(LogLevel::Info);			//write to std::cout with min_level INFO
 
 	//std::ofstream file("log.txt", std::ios::app);
-	//Logger inst(LogLevel::Info, file);			//write to file with min_level INFO
-													//don't close file, Logger do it automatically
+	//Logger inst(LogLevel::Info, file);		//write to file with min_level INFO
+							//don't close file, Logger do it automatically
 	
 	inst << LogLevel::Error << "Hello, world! #" << 555 << 123;
 	inst << LogLevel::Warning << "123";
