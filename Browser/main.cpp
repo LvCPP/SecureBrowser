4d1a44843a9aaa480465bb8eb4e723c245e893d2
SvyatDemn <cjiabi1994@gmail.com>
2017-05-02 18:55:46 +0300

    Login from Browser

--- Browser/main.cpp
+++ Browser/main.cpp
@@ -1,12 +1,20 @@
 #include "browser.h"
+#include <windows.h>
+#include "LoginApp.hpp"
 #include <QtWidgets/QApplication>
 
 using namespace SecureBrowser;
+using namespace BrowserLogin;
 
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
+	
 	Browser w;
 	w.showMaximized();
+
+	LoginApp app;
+	app.show();
+
 	return a.exec();
 }
