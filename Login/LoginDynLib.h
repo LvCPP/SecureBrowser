e03d650fcf6cb3a264dd6f227e423c25b1879ba7
dillingerUA <arsen.peleh@gmail.com>
2017-05-02 16:07:46 +0300

    Changes in project structure

--- Login/LoginDynLib.h
+++ Login/LoginDynLib.h
@@ -3,9 +3,11 @@
 
 namespace BrowserLogin
 {
+
 #ifdef LOGIN_LIB
-#define LOGIN_API __declspec(dllimport)
-#else
 #define LOGIN_API __declspec(dllexport)
+#else
+#define LOGIN_API __declspec(dllimport)
 #endif
+
 }
