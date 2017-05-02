5fb364cb742524b83749ad002d24b85bfdc1ab93
SvyatDemn <cjiabi1994@gmail.com>
2017-05-02 13:08:52 +0300

    clean code

--- Browser/browser.cpp
+++ Browser/browser.cpp
@@ -6,26 +6,26 @@ Browser::Browser(QWidget *parent)
 	: QWidget(parent), ui_(std::make_unique<Ui::BrowserClass>())
 {
 	ui_->setupUi(this);
-	connect(ui_->LineEdit, &QLineEdit::returnPressed, this, &Browser::SlotEnter);
-	connect(ui_->BtnBack, &QPushButton::clicked, ui_->WebView, &QWebEngineView::back);
-	connect(ui_->BtnForward, &QPushButton::clicked, ui_->WebView, &QWebEngineView::forward);
-	connect(ui_->BtnReload, &QPushButton::clicked, ui_->WebView, &QWebEngineView::reload);
-	connect(ui_->BtnStop, &QPushButton::clicked, ui_->WebView, &QWebEngineView::stop);
-	connect(ui_->WebView, &QWebEngineView::loadProgress, ui_->ProgressBar, &QProgressBar::setValue);
-	connect(ui_->WebView, &QWebEngineView::urlChanged, this, &Browser::SetUrl);
+	connect(ui_->line_edit, &QLineEdit::returnPressed, this, &Browser::SlotEnter);
+	connect(ui_->push_btn_back, &QPushButton::clicked, ui_->web_view, &QWebEngineView::back);
+	connect(ui_->push_btn_forward, &QPushButton::clicked, ui_->web_view, &QWebEngineView::forward);
+	connect(ui_->push_btn_reload, &QPushButton::clicked, ui_->web_view, &QWebEngineView::reload);
+	connect(ui_->push_btn_stop, &QPushButton::clicked, ui_->web_view, &QWebEngineView::stop);
+	connect(ui_->web_view, &QWebEngineView::loadProgress, ui_->load_progress_page, &QProgressBar::setValue);
+	connect(ui_->web_view, &QWebEngineView::urlChanged, this, &Browser::SetUrl);
 }
 
 void Browser::SlotEnter()
 {
-	QString uri = ui_->LineEdit->text();
+	QString uri = ui_->line_edit->text();
 	if (!(uri.startsWith("http://")))
 	{
 		uri = "http://" + uri;
 	}
-	ui_->WebView->load(uri);
+	ui_->web_view->load(uri);
 }
 
 void Browser::SetUrl(const QUrl &url)
 {
-	ui_->LineEdit->setText(url.toString());
+	ui_->line_edit->setText(url.toString());
 }
