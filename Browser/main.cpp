7a1840dc77e147b09e9288d70cea2bef224e8d55
SvyatDemn <cjiabi1994@gmail.com>
2017-05-10 13:37:27 +0300

    clean code

--- Browser/main.cpp
+++ Browser/main.cpp
@@ -1,18 +1,12 @@
 #include "browser.h"
 #include <windows.h>
-#include "LoginApp.hpp"
 #include <QtWidgets/QApplication>
 
 using namespace SecureBrowser;
-using namespace BrowserLogin;
 
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
-
-	//LoginApp app;
-	//if (!app.exec())
-	//	return 0; //user no authenticated
 
 	Browser w;
 	w.showMaximized();
