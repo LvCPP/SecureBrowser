7a70a573785b7eedc19cc0c4f2a85f8fcddb9c7a
StMarian <stychuk.marian@gmail.com>
2017-06-20 14:59:16 +0300

    Fix test project

--- Tools/LoginDialog_test/main.cpp
+++ Tools/LoginDialog_test/main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
 	An<WebCamController>()->RegisterForDeviceNotification();
 
 	QApplication a(argc, argv);
-
-	LoginDialog login_dialog("lv-28-226215", "Z7_7_P", argv[0]);
+	
+	LoginDialog login_dialog("lv-28-226215", "Z7_7_P", "3257", "231", argv[0]);
 	login_dialog.show();
 	
 	int result = a.exec();
